cea35b706e1860f031392ea6bb2187a440c37e20
egbertli <egbert1121@gmail.com>
2018-01-07 02:59:28 +1100

    start review sql, now accelerate reviewing

---
+++ Data_Analysis_With_SQL_and_PostGreSQL/01-SQL_Statement_Fundamentals/MIN_MAX_AVG.sql
@@ -0,0 +1,24 @@
+﻿-- Agg Functions
+
+-- Get average amout
+SELECT AVG(amount) FROM payment;
+
+-- Using Round to limit the decimal point
+SELECT ROUND(AVG(amount), 5) FROM payment;
+
+-- Get minimum amount
+SELECT MIN(amount) FROM payment;
+
+-- MAX
+SELECT MAX(amount) FROM payment;
+
+-- ORDER BY
+SELECT amount FROM payment
+ORDER BY amount;
+
+-- COUNT
+SELECT COUNT(amount) FROM payment
+WHERE amount=0.00;
+
+-- SUM
+SELECT SUM(amount) FROM payment;
