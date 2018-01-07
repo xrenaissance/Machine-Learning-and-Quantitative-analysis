9cef0d2e6b9e6f5634678c7c7ec3128fbaddbedf
egbertli <egbert1121@gmail.com>
2018-01-07 23:24:10 +1100

    almost finished review

---
+++ Data_Analysis_With_SQL_and_PostGreSQL/05-Advanced-SQL-Commands/Advanced_SQL_command.sql
@@ -0,0 +1,28 @@
+﻿-- Extract
+SELECT payment_date FROM payment;
+
+SELECT customer_id ,extract(day from payment_date) AS Day FROM payment;
+
+
+-- get total spent for each month	
+SELECT SUM(amount) AS total_spend, extract(month from payment_date) AS month
+FROM payment
+GROUP BY month
+ORDER BY total_spend DESC; 
+
+--- Math function
+SELECT customer_id+rental_id AS new_id
+FROM payment;
+
+SELECT rental_id/customer_id AS new_id
+FROM payment;
+
+-- String Function
+SELECT * FROM customer;
+
+-- concat first name and last name as full name
+SELECT first_name ||' '|| last_name AS full_name FROM customer;
+
+SELECT first_name, char_length(first_name) FROM customer;
+
+-- Subquery
