b92594d4761f49f9a53fece1a997c06a0f3779e1
egbertli <egbert1121@gmail.com>
2018-01-08 04:03:11 +1100

    finished last part

--- Data_Analysis_With_SQL_and_PostGreSQL/05-Advanced-SQL-Commands/Advanced_SQL_command.sql
+++ Data_Analysis_With_SQL_and_PostGreSQL/05-Advanced-SQL-Commands/Advanced_SQL_command.sql
@@ -43,6 +43,52 @@ select * from rental;
 select * from inventory limit 1;
 
 
+SELECT film_id, title
+FROM film
+WHERE film_id IN
+(SELECT inventory.film_id
+FROM rental
+INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+WHERE return_date BETWEEN '2005-05-29' AND '2005-05-30');
+
+
+/**
+* SELF-JOIN
+**/
+
+-- find out which employees are from the same location as the employee named joe
+/**
+* Using subquery
+*
+* Interview question:  https://blog.sqlauthority.com/2016/03/27/self-join-explain-example-interview-question-week-064/
+**/
+SELECT employee_name FROM employee
+WHERE employee_location IN
+(SELECT employee_location FROM employee WHERE employee_name="Joe")
+
+-- Use self join
+SELECT e1.employee_name
+FROM employee AS e1, employee AS e2
+WHERE
+e1.employee_location = e2.employee_location
+AND e2.employee_name = "Joe";
+
+
+-- find out customer whoes first name matches last name
+
+SELECT c1.customer_id,c1.first_name, c1.last_name, c2.first_name, c2.last_name
+FROM customer AS c1, customer AS c2
+WHERE c1.first_name = c2.last_name
+ORDER BY c1.customer_id;
+
+
+
+
+
+
+
+
+
 
 
 
