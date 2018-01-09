e0e93f9f3a9a2aeef33a3078ad77ebc677aac98f
egbertli <egbert1121@gmail.com>
2018-01-10 03:37:44 +1100

    quick review about view

---
+++ Data_Analysis_With_SQL_and_PostGreSQL/09-Views/VIEW.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW customer_info AS 
+SELECT first_name, last_name, email, address, phone
+FROM customer
+JOIN address
+ON customer.address_id = address.address_id;
+
+SELECT * FROM customer_info;
+
+
+-- ALTER VIEW name_of_view RENAME TO new_name
+-- DROP VIEW IF EXISTS name_of_view;
+ALTER VIEW customer_info RENAME TO customer_master_list;
+
+SELECT * FROM customer_master_list;
+
+DROP VIEW customer_master_list;
